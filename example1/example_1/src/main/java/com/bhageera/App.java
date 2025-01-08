e973e10bfa9e30aab2d06dc3095ef75e4e7bd07c
deepanshudua555 <deepanshudua555@gmail.com>
2025-01-09 01:17:54 +0530

    added dependency spring-context in pom.xml

---
+++ example1/example_1/src/main/java/com/bhageera/App.java
@@ -0,0 +1,13 @@
+package com.bhageera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Deepanshu!" );
+    }
+}
